5cac7386d0e8ea4717d5c2f57d40b01106ffe141
minimallinux <anthonymcgowan1@gmail.com>
2018-03-29 09:38:33 +0100

    Update

--- app/setup.php
+++ app/setup.php
@@ -51,6 +51,8 @@ add_action('after_setup_theme', function () {
      * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
      */
     add_theme_support('post-thumbnails');
+    set_post_thumbnail_size( $width, $height, $crop ); 
+
 
     /**
      * Enable HTML5 markup support
